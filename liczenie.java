a4526e884d695b3f92f595afdb7518610b228ca7
pucpan <danielsiw@gmail.com>
2017-03-19 12:12:57 +0100

    Update liczenie.java

--- liczenie.java
+++ liczenie.java
@@ -6,21 +6,21 @@ import org.apache.commons.lang3.StringUtils;
 
 public class liczenie {
 
-public static int wynik(String pom){
+public static void wynik(String pom){
 	double wynik=0;
 	Scanner zmienna = new Scanner(System.in);
 	
-//	do{
+
 		 int pom2=0,licznik=0, licznik2=0;
-		 int []liczba=new int[1000];
-		 char[] znak=new char[1000];
+		 int []liczba=new int[1000]; //dalem ograniczona ilosc przechowywania liczb, nie znam jeszcze w javie list
+		 char[] znak=new char[1000]; // to samo co powyżej
 		 
 		
 		 
-		 if(!pom.equals("koniec")){
-		 //System.out.println("Wynik to działania:"+pom+"=");
+		 if(!pom.equals("koniec")){   //jesli wykryje slowo koniec metoda przestanie dzialac i wroci do menu
+		
 	
-		
+		//w tej petli dziele sobie stringa
 		 for(int i=0;i<pom.length();i++){
 		
 			 
@@ -36,13 +36,12 @@ public static int wynik(String pom){
 			}else{
 				if(i>0){
 				System.out.println("Znak znaleziony to: "+ pom.substring(i-1, i));
-				//System.out.println("Wypisuje to co zapisuje do zmiennej"+ pom.charAt(i-1)+ "licznik2 wynosi: "+licznik2);
 				znak[licznik2]=pom.charAt(i-1);
 				licznik2++;
 				
 				}
 				pom2=i;
-			}
+			} // ponizej if przypisuje liczbe, gdyz na koncu zawsze bedzie liczba. Nie mozna zakonczyc znakiem działania
 			if(i+1==pom.length()){
 				System.out.println("OStastnia liczba:"+ pom.substring(pom2, i+1));
 			liczba[licznik]=Integer.parseInt(pom.substring(pom2, i+1));
@@ -51,9 +50,7 @@ public static int wynik(String pom){
 			
 		 }
 		 
-		 
-		 
-	//	 for(int i=0;i<licznik2;i++)
+//wykonuje obliczenia bez sprawdzania kolejnosci działan matematycznych		 
 wynik=liczba[0];
 		 for(int i=0;i<licznik2;i++){
 			 if( znak[i]=='+' ){
@@ -70,27 +67,11 @@ wynik=liczba[0];
 							}
 				
 			}
-		 System.out.println("Wynik to: "+ wynik);
-		
-
-		 
-	/*	 System.out.println("Wyswietlam zapisane liczby: ");
-		 for(int i=0;i<licznik;i++){
-			 System.out.println(i+". "+ liczba[i]);
-			 
-		 }
-		 
-		 System.out.println("Wyswietlam zapisane znaki: ");			 
-for(int i=0;i<licznik2;i++){
-System.out.println(i +". "+ znak[i]);					 
-		 }
-		 */
+		 System.out.println("Wynik to: "+ wynik); //wyswietlam wynik
 		 
 		 }	 
-	// }while(!pom.equals("koniec"));
+
 	
-	
-	return 0;
 }	
 
 }
